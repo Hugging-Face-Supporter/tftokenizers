8cd049f7f61df4239b1797c3bcd1f83bf2e95f61
MarkusSagen <markus.john.sagen@gmail.com>
2022-01-21 17:39:37 +0100

    Clean up imports

--- tftokenizers/__init__.py
+++ tftokenizers/__init__.py
@@ -1,6 +1,6 @@
 """Use Huggingface Transformer and Tokenizers as Tensorflow Resuable SavedModels."""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .detect import detect_and_load_tokenizer as detect_and_load_tokenizer
 from .detect import find_tf_base_tokenizer as find_tf_base_tokenizer
@@ -10,4 +10,3 @@ from .file import get_vocab_from_path as get_vocab_from_path
 from .model import TFModel as TFModel
 from .tokenizer import TFAutoTokenizer as TFAutoTokenizer
 from .tokenizer import TFTokenizerBase as TFTokenizerBase
-from .types import *
