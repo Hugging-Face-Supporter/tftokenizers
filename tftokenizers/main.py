d7c41488b0be4a28ab3a82f272e2f57bc6ad9caf
MarkusSagen <markus.john.sagen@gmail.com>
2022-03-12 23:44:32 +0100

    :fire: Cleanup

--- tftokenizers/main.py
+++ tftokenizers/main.py
@@ -1,30 +1,80 @@
 import tensorflow as tf
-from transformers import TFAutoModel
+import tensorflow_text as text
+from transformers import AutoTokenizer, TFAutoModel
+from transformers.utils.logging import set_verbosity_error
 
+from tftokenizers.file import get_filename_from_path, get_vocab_from_path, load_json
 from tftokenizers.model import TFModel
-from tftokenizers.tokenizer import TFAutoTokenizer
+from tftokenizers.tokenizer import TFAutoTokenizer, TFTokenizerBase
+
+set_verbosity_error()
+tf.get_logger().setLevel("ERROR")
 
 if __name__ == "__main__":
     # Load base models from Huggingface
     pretrained_model_name = "bert-base-cased"
-    tokenizer = TFAutoTokenizer(pretrained_model_name)
+    pretrained_model_name = "distilbert-base-cased"
+    pretrained_model_name = "distilbert-base-multilingual-cased"
+    pretrained_model_name = "bert-base-multilingual-cased"
+    pretrained_model_name = "KB/bert-base-swedish-cased"
+
+    tf_tokenizer = TFAutoTokenizer.from_pretrained(pretrained_model_name)
+    hf_tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
     model = TFAutoModel.from_pretrained(pretrained_model_name)
 
-    # Create a Reusable SavedModel
-    custom_model = TFModel(model=model, tokenizer=tokenizer)
+    # Three ways to load the TF tokenizer
+    # a) by model_name
+    tf_tokenizer = TFAutoTokenizer.from_pretrained(pretrained_model_name)
 
-    # Use model in inference or training
+    # b) bundled with the model, similar to TFHub
+    model = TFAutoModel.from_pretrained(pretrained_model_name)
+    custom_model = TFModel(model=model, tokenizer=tf_tokenizer)
+
+    # c) from vocab file
+    PATH = "saved_tokenizers/bert-base-uncased"
+    vocab = get_vocab_from_path(PATH)
+    vocab_path = get_filename_from_path(PATH, "vocab")
+
+    config = load_json(f"{PATH}/tokenizer_config.json")
+    tokenizer_spec = load_json(f"{PATH}/tokenizer.json")
+    special_tokens_map = load_json(f"{PATH}/special_tokens_map.json")
+
+    tokenizer_base_params = dict(lower_case=True, token_out_type=tf.int64)
+    tokenizer_base = text.BertTokenizer(vocab_path, **tokenizer_base_params)
+    custom_tokenizer = TFTokenizerBase(
+        vocab_path=vocab_path,
+        tokenizer_base=tokenizer_base,
+        hf_spec=tokenizer_spec,
+        config=config,
+    )
+
+    # Usage
+    # TF tokenizer have the same interface as HF transformers
+    sentence = "Huggingface to Tensorflow tokenizers"
+    print(tf_tokenizer.batch_encode_plus([sentence])["input_ids"])
+    print(
+        hf_tokenizer.batch_encode_plus(
+            [sentence],
+            padding=True,
+            truncation=True,
+            return_token_type_ids=False,
+            return_tensors="tf",
+        )["input_ids"]
+    )
+
+    # Create a Reusable SavedModel
+    custom_model = TFModel(model=model, tokenizer=tf_tokenizer)
+
     # With the tokenizer in Tensorflow, it can handle Tensors and python string
     string_tf = tf.constant(["Hello from Tensorflow"])
+    output = custom_model(string_tf)
+
+    # And pass input for either inference (Default) or training
+    output = custom_model(string_tf, training=False)  # inference
+    output = custom_model(string_tf, training=True)  # training
+
+    # Or pass a batch of sentences, similar to Huggingface
     s1 = "sponge bob squarepants is an avenger"
     s2 = "Huggingface to Tensorflow tokenizers"
     s3 = "HelLo!"
-
-    output = custom_model(string_tf)
     output = custom_model([s1, s2, s3])
-
-    # You can also pass arguments, similar how they are named from Huggingface
-    output = custom_model(
-        [s1, s2, s3],
-    )
-    print(output)
--- tftokenizers/model.py
+++ tftokenizers/model.py
@@ -1,18 +1,36 @@
-from typing import Optional
+from typing import Dict, List, Union
 
 import tensorflow as tf
 import tensorflow.keras as keras
-import tensorflow_hub as hub
-import tensorflow_text as text
 from transformers import AutoTokenizer, TFAutoModel
 from transformers.utils.logging import set_verbosity_error
 
-from tftokenizers.file import get_filename_from_path, get_vocab_from_path, load_json
-from tftokenizers.tokenizer import TFAutoTokenizer, TFTokenizerBase
-from tftokenizers.types import PaddingStrategies
+from tftokenizers.tokenizer import TFAutoTokenizer
 
 set_verbosity_error()
 tf.get_logger().setLevel("ERROR")
+
+
+def compare_models(pretrained_model_name: str, inputs: Union[str, List[str]]):
+    """Helper function for comparing TF and HF tokenizers."""
+
+    model = TFAutoModel.from_pretrained(pretrained_model_name)
+
+    tf_tokenizer = TFAutoTokenizer.from_pretrained(pretrained_model_name)
+    tf_hub_model = TFModel(model=model, tokenizer=tf_tokenizer)
+    tf_output = tf_hub_model(inputs, training=False)
+
+    hf_tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+    hf_tokens: Dict[str, tf.Tensor] = hf_tokenizer.batch_encode_plus(
+        inputs,
+        padding=True,
+        truncation=True,
+        return_token_type_ids=False,
+        return_tensors="tf",
+    )
+    hf_output = model(hf_tokens)[0]  # "last_hidden_state"
+
+    return {"tf_output": tf_output, "hf_output": hf_output}
 
 
 class TFModel(keras.layers.Layer):
@@ -35,60 +53,3 @@ class TFModel(keras.layers.Layer):
         embeddings = self.model(tokens, training=training)
         last_hidden_out = embeddings[0]
         return last_hidden_out
-
-
-if __name__ == "__main__":
-    PATH = "saved_tokenizers/bert-base-uncased"
-    vocab = get_vocab_from_path(PATH)
-    vocab_path = get_filename_from_path(PATH, "vocab")
-
-    config = load_json(f"{PATH}/tokenizer_config.json")
-    tokenizer_spec = load_json(f"{PATH}/tokenizer.json")
-    special_tokens_map = load_json(f"{PATH}/special_tokens_map.json")
-
-    tokenizer_base_params = dict(lower_case=True, token_out_type=tf.int64)
-    tokenizer_base = text.BertTokenizer(vocab_path, **tokenizer_base_params)
-
-    model = TFAutoModel.from_pretrained("bert-base-uncased")
-    hf_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-
-    s1 = "sponge bob squarepants is an avenger"
-    s2 = "Huggingface to Tensorflow tokenizers"
-    s3 = "HelLo!"
-
-    # Define out custom tokenizer
-    max_length = 512
-    custom_tokenizer = TFTokenizerBase(
-        vocab_path=vocab_path,
-        tokenizer_base=tokenizer_base,
-        hf_spec=tokenizer_spec,
-        config=config,
-    )
-    custom_tokenizer = TFAutoTokenizer.from_pretrained("bert-base-uncased")
-
-    # Building our custom TF model pipeline
-    custom_model = TFModel(model, custom_tokenizer)
-    output = custom_model(
-        [s1, s2, s3],
-    )
-
-    # Compare against the Huggingface implementation
-    hf_tokens = hf_tokenizer.batch_encode_plus(
-        [s1, s2, s3],
-        padding="max_length",
-        truncation=True,
-        max_length=max_length,
-        return_tensors="tf",
-    )
-
-    print("\n\nmodel output")
-    hf_output = model(hf_tokens)[0]
-    print(output)
-    print(hf_output)
-
-    # Save custom TF model and re-use it
-    model_name = "bert_reusable_tf"
-    tf.saved_model.save(custom_model, model_name)
-    reloaded_model = tf.saved_model.load(model_name)
-    output = custom_model([s1, s2, s3])
-    print(output)
